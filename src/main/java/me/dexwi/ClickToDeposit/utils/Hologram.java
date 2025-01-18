897fa27666d148a914040a32fc3d7da3a410732d
imfabulousxd <dexwi.me@gmail.com>
2025-01-18 22:36:47 +0330

    Fixed armor stand being in the way of clicking

--- src/main/java/me/dexwi/ClickToDeposit/utils/Hologram.java
+++ src/main/java/me/dexwi/ClickToDeposit/utils/Hologram.java
@@ -3,10 +3,10 @@ package me.dexwi.ClickToDeposit.utils;
 import com.andrei1058.bedwars.api.language.Language;
 import me.dexwi.ClickToDeposit.ClickToDeposit;
 import net.minecraft.server.v1_8_R3.*;
-import org.apache.commons.codec.language.bm.Lang;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.craftbukkit.v1_8_R3.CraftWorld;
+import org.bukkit.craftbukkit.v1_8_R3.entity.CraftArmorStand;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -18,21 +18,40 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Hologram implements Listener {
-    private final EntityArmorStand armorStand;
+//    private final EntityArmorStand armorStand;
+    private final CraftArmorStand armorStand;
     private final Set<Player> displayingFor = new HashSet<>();
     private final String path;
 
     public Hologram(Location location, String messagePath) {
         path = messagePath;
-        armorStand = new EntityArmorStand(
+//        armorStand = new EntityArmorStand(
+//                ((CraftWorld) location.getWorld()).getHandle()
+//        );
+//        armorStand.setLocation(location.getX() + 0.5,
+//                location.getY() - 1,
+//                location.getZ() + 0.5, 0, 0);
+//        armorStand.setCustomNameVisible(true);
+//        armorStand.setInvisible(true);
+//        armorStand.setGravity(false);
+//        armorStand.setBasePlate(false);
+//        armorStand.setArms(false);
+        EntityArmorStand entityArmorStand = new EntityArmorStand(
                 ((CraftWorld) location.getWorld()).getHandle()
         );
-        armorStand.setLocation(location.getX() + 0.5,
-                location.getY() - 1,
+        entityArmorStand.setLocation(location.getX() + 0.5,
+                location.getY() + 1,
                 location.getZ() + 0.5, 0, 0);
-        armorStand.setCustomNameVisible(true);
-        armorStand.setInvisible(true);
+        armorStand = new CraftArmorStand(((CraftWorld) location.getWorld()).getHandle().getServer(),
+                entityArmorStand);
         armorStand.setGravity(false);
+        armorStand.setRemoveWhenFarAway(false);
+        armorStand.setVisible(false);
+        armorStand.setCanPickupItems(false);
+        armorStand.setArms(false);
+        armorStand.setBasePlate(false);
+        armorStand.setCustomNameVisible(true);
+        armorStand.setMarker(true);
 
         Bukkit.getPluginManager().registerEvents(this, ClickToDeposit.getInstance());
     }
@@ -41,7 +60,7 @@ public class Hologram implements Listener {
         armorStand.setCustomName(
                 Language.getMsg(player, path)
         );
-        PacketPlayOutSpawnEntityLiving packet = new PacketPlayOutSpawnEntityLiving(armorStand);
+        PacketPlayOutSpawnEntityLiving packet = new PacketPlayOutSpawnEntityLiving(armorStand.getHandle());
         ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
         displayingFor.add(player);
     }
@@ -49,13 +68,13 @@ public class Hologram implements Listener {
     public void destroyFor(Player player) {
         boolean removed = displayingFor.remove(player);
         if (removed) {
-            PacketPlayOutEntityDestroy packet = new PacketPlayOutEntityDestroy(armorStand.getId());
+            PacketPlayOutEntityDestroy packet = new PacketPlayOutEntityDestroy(armorStand.getHandle().getId());
             ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
         }
     }
 
     private void sendHologramDeletionPacket(Player player) {
-        PacketPlayOutEntityDestroy packet = new PacketPlayOutEntityDestroy(armorStand.getId());
+        PacketPlayOutEntityDestroy packet = new PacketPlayOutEntityDestroy(armorStand.getHandle().getId());
         ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
     }
 
@@ -69,7 +88,7 @@ public class Hologram implements Listener {
 
     @EventHandler
     public void playerLeftWorld(PlayerChangedWorldEvent event) {
-        if (event.getFrom() != armorStand.world) {
+        if (event.getFrom() != armorStand.getHandle().world) {
             return;
         }
         displayingFor.remove(event.getPlayer());
