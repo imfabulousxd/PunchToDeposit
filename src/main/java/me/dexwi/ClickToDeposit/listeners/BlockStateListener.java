86df83e76fb4dd5d9272e88ec9d8574073c8bf28
imfabulousxd <dexwi.me@gmail.com>
2025-01-18 23:00:48 +0330

    Changed the mechanics of the mod.

--- src/main/java/me/dexwi/ClickToDeposit/listeners/BlockStateListener.java
+++ src/main/java/me/dexwi/ClickToDeposit/listeners/BlockStateListener.java
@@ -3,10 +3,12 @@ package me.dexwi.ClickToDeposit.listeners;
 import com.andrei1058.bedwars.api.BedWars;
 import com.andrei1058.bedwars.api.arena.IArena;
 import com.andrei1058.bedwars.api.arena.team.ITeam;
-import com.andrei1058.bedwars.api.configuration.ConfigPath;
 import com.andrei1058.bedwars.api.language.Language;
 import me.dexwi.ClickToDeposit.ClickToDeposit;
 import me.dexwi.ClickToDeposit.Messages;
+import me.dexwi.ClickToDeposit.utils.Bedwars;
+import me.dexwi.ClickToDeposit.utils.Blocks;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
@@ -19,8 +21,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.HashMap;
+import java.util.Map;
 
-import static me.dexwi.ClickToDeposit.ClickToDeposit.log;
+import static me.dexwi.ClickToDeposit.ClickToDeposit.gameChestLocations;
 
 public class BlockStateListener implements Listener {
     private final HashMap<Material, String> materialNames = new HashMap<>();
@@ -108,19 +111,15 @@ public class BlockStateListener implements Listener {
     }
 
     private static boolean canOpenChest(IArena a, Block block, Player p) {
-        ITeam owner = null;
-        int isRad = a.getConfig().getInt(ConfigPath.ARENA_ISLAND_RADIUS);
-        for (ITeam t : a.getTeams()) {
-            if (t.getSpawn().distance(block.getLocation()) <= isRad) {
-                owner = t;
+        ITeam playerTeam = a.getTeam(p);
+        for (Map.Entry<ITeam, Location> entry: gameChestLocations.get(a).entrySet()) {
+            if (
+                    Blocks.locationEquals(entry.getValue(), block.getLocation())
+            ) {
+                return playerTeam == entry.getKey() || Bedwars.isEliminated(entry.getKey());
             }
         }
-        if (owner != null) {
-            if (!owner.isMember(p)) {
-                return owner.getMembers().isEmpty() && owner.isBedDestroyed();
-            }
-        }
-        return true;
+        return false;
     }
 
     private static boolean itemStackFitsInInventory(Inventory inventory, ItemStack item) {
---
+++ src/main/java/me/dexwi/ClickToDeposit/utils/Blocks.java
@@ -0,0 +1,13 @@
+package me.dexwi.ClickToDeposit.utils;
+
+import org.bukkit.Location;
+
+public class Blocks {
+    public static boolean locationEquals(Location l1, Location l2) {
+        return (
+                l1.getX() == l2.getX() &&
+                l1.getY() == l2.getY() &&
+                l1.getZ() == l2.getZ()
+        );
+    }
+}
