47564826488b8e59da125ed8804b423d85f89c87
iRango <154560742+iRxngo@users.noreply.github.com>
2025-01-27 08:57:44 -0800

    Update Messages.java
    Hologram

--- src/main/java/me/dexwi/ClickToDeposit/Messages.java
+++ src/main/java/me/dexwi/ClickToDeposit/Messages.java
@@ -10,7 +10,8 @@ public class Messages {
     public static final String DEPOSIT_FAILURE_CHEST_FULL = PATH + "deposit-failure-chest-full";
     public static final String DEPOSIT_FAILURE_MINIMUM_ITEM = PATH + "deposit-failure-minimum-item";
     public static final String DEPOSIT_FAILURE_NOT_ELIMINATED_TEAM_CHEST = PATH + "deposit-failure-not-eliminated-team-chest";
-    public static final String CHEST_HOLOGRAM = PATH + "chest-hologram";
+    public static final String CHEST_HOLOGRAM_1 = PATH + "chest-hologram-1";
+    public static final String CHEST_HOLOGRAM_2 = PATH + "chest-hologram-2";
 
     public static final String ITEM_NAMES_PATH = PATH + "items.";
     public static final String ITEM_NAME_IRON_INGOT = ITEM_NAMES_PATH + "iron-ingot";
@@ -42,13 +43,15 @@ public class Messages {
 
     public static void setupMessages() {
         for (Language l: Language.getLanguages()) {
-            addDefault(l, DEPOSIT_SUCCESS_TEAM_CHEST, "&7You deposited x{item_amount} &r{item_name}&r&7 into your team chest!");
+            addDefault(l, DEPOSIT_SUCCESS_TEAM_CHEST, "&7You deposited x{item_amount} &r{item_name}&r&7 into your &bTeam chest!");
             addDefault(l, DEPOSIT_SUCCESS_OTHER_TEAM_CHEST, "&7You deposited x{item_amount} &r{item_name}&r&7 into {team_color}{team_name} team&r&7's chest!");
-            addDefault(l, DEPOSIT_SUCCESS_ENDER_CHEST, "&7You deposited x{item_amount} &r{item_name}&r&7 into your ender chest!");
+            addDefault(l, DEPOSIT_SUCCESS_ENDER_CHEST, "&7You deposited x{item_amount} &r{item_name}&r&7 into your &dEnder chest!");
             addDefault(l, DEPOSIT_FAILURE_CHEST_FULL, "&7There's not enough space to deposit x{item_amount} &r{item_name}&r&7 into this chest!");
             addDefault(l, DEPOSIT_FAILURE_MINIMUM_ITEM, "&7You need at least {item_minimum_amount} of this type to deposit.");
             addDefault(l, DEPOSIT_FAILURE_NOT_ELIMINATED_TEAM_CHEST, "&cYou cannot deposit into this chest as {team_color}{team_name} Team&r&c has not been eliminated!");
-            addDefault(l, CHEST_HOLOGRAM, "&lPunch to deposit!");
+
+            addDefault(l, CHEST_HOLOGRAM_1, "&7PUNCH TO");
+            addDefault(l, CHEST_HOLOGRAM_2, "&7DEPOSIT");
 
             addDefault(l, ITEM_NAME_IRON_INGOT, "&r&fIron Ingot");
             addDefault(l, ITEM_NAME_GOLD_INGOT, "&r&6Gold Ingot");
